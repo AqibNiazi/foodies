ec11337f6fe47f63c54d61705585e60c764c359f
AqibNiazi <aqibjaved5201@gmail.com>
2024-09-06 11:15:11 +0500

    initial project setup of foodies application completed

---
+++ app/meals/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const MealsPage = () => {
+  return (
+    <div>
+      <Link href="/meals/share">Share Meals</Link>
+    </div>
+  );
+};
+
+export default MealsPage;
---
+++ components/main-header/index.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import IconImg from "@/assets/logo.png";
+import styles from "./main-header.module.css";
+import Image from "next/image";
+const MainHeader = () => {
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
+        <Image src={IconImg} alt="a plate with food on it." />
+        Foodies
+      </Link>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
