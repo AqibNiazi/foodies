d5b921790d6c44b41e4d2a16fb5af24b533d4881
AqibNiazi <aqibjaved5201@gmail.com>
2024-09-10 13:27:53 +0500

    sqlite data base setup and meals page created and showing meals also loading state added on the meals

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,11 +1,28 @@
-import Link from "next/link";
 import React from "react";
-
-const MealsPage = () => {
+import styles from "./Page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
-    <div>
-      <Link href="/meals/share">Share Meals</Link>
-    </div>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself.It is easy and fun.
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe.</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 };
 
