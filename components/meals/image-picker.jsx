0b845e31d1aef470c4281408259f663aa8b5a2ae
AqibNiazi <aqibjaved5201@gmail.com>
2024-09-19 20:45:57 +0500

    working on input red

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import styles from "./image-picker.module.css";
 const ImagePicker = ({ label, id, name }) => {
+  const imageInputRef = useRef();
+  const handlePickClick = () => {};
   return (
     <div className={styles.picker}>
       <label htmlFor={id}>{label}</label>
@@ -12,7 +15,11 @@ const ImagePicker = ({ label, id, name }) => {
           accept="image/png, image/jpeg"
           name={name}
         />
-        <button className={styles.button} type="button">
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePickClick}
+        >
           {" "}
           Pick an Image
         </button>
